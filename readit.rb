6ce653b08b70d273f2a006ec8d058234ed2f36fd
Ben Livingood <livingood@gmail.com>
2010-01-15 13:38:13 -0700

    ruby comes w/ its own, less refined parser

--- readit.rb
+++ readit.rb
@@ -1,7 +1,8 @@
-require 'simple-rss'
+require 'rss'
 require 'open-uri'
 
-rss = SimpleRSS.parse open('http://twitter.com/statuses/user_timeline/15827231.rss')
+#rss = SimpleRSS.parse open('http://twitter.com/statuses/user_timeline/15827231.rss')
+rss = RSS::Parser.parse open('http://twitter.com/statuses/user_timeline/15827231.rss')
 
 puts rss.channel.title
 rss.items.each do |i|
