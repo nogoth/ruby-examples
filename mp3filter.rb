b08beb6d3921b7be22f97d7d3b895660d0623130
Ben Livingood <livingood@gmail.com>
2012-03-18 15:52:14 -0600

    whitespace is evil

--- mp3filter.rb
+++ mp3filter.rb
@@ -5,14 +5,15 @@ Artist = Regexp.new(ARGV[0]||"Coulton", 'i')
 # read and display infos & tags
 STDIN.readlines.each do |line|
  begin
+
   Mp3Info.open(line[0..-2]) do |mp3info|
     if mp3info.tag#.artist
-						if mp3info.tag.to_s.match(Artist)
-										puts line
-						end
-
-		end
+     if mp3info.tag.to_s.match(Artist)
+	puts line
+     end
+    end
   end
+
  rescue
    #do nothing of value
  end
